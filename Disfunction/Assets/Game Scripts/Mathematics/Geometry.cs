9d0d348cb9ae0f240cb4b9e9837ccc2f49452015
ashish.r <ashishphilip186@gmail.com>
2024-11-16 11:56:31 +0530

    Developed getting distance in the scene

---
+++ Disfunction/Assets/Game Scripts/Mathematics/Geometry.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geometry : MonoBehaviour
+{
+    public static float GetDistance(Vector3 from, Vector3 to)
+    {
+       return Mathf.Sqrt( Square((from.x - to.x)) + Square((from.y - to.y)) + Square((from.z - to.z))  );
+    }
+
+    public static float Square(float number)
+    {
+        return number * number;
+    }
+}
