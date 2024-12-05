e3ef82f7ac312d55763fa79c87876f5feeaec11e
ashish.r <ashishphilip1886@gmail.com>
2024-12-06 00:39:59 +0530

    Mod depclear

--- depclear.sh
+++ depclear.sh
@@ -6,7 +6,7 @@ DEST_DIR="$ASSETS_DIR/$DEP_DIR"
 HAS_META="false"
 
 if [ -d "$DEST_DIR" ]; then
-    sudo rm -rf "$DEST_DIR"/*
+    rm -rf "$DEST_DIR"/*
     echo "Cleared all dependencies ..."
 else
     echo "Nothing to clear ..."
