2b6c881277d2589cd4b616a3902093ceb8cb69e4
ruchithdhoni <ruchithdhoni@gmail.com>
2025-02-12 02:37:06 +0530

    Added Upward and downward motion and rappelling can be paused at any point and the rappelling direction can be reversed

--- Disfunction/Assets/Game Scripts/Development/GadgetScripts/RappellingRopeScripts/rappellingRopeScript.cs
+++ Disfunction/Assets/Game Scripts/Development/GadgetScripts/RappellingRopeScripts/rappellingRopeScript.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class RappellingRopeScript : MonoBehaviour
 {
     public float rappellingSpeed = 3f;
+    public KeyCode keycodeForUp = KeyCode.Q;
+    public KeyCode keycodeForDown = KeyCode.E;
     private CharacterController characterController;
-    private bool isRappelling = false;
+    private bool isRappellingUp = false;
+    private bool isRappellingDown = false;
+    private Vector3 initialPosition;
 
     void Start()
     {
+        initialPosition = transform.position;
         characterController = GetComponent<CharacterController>();
     }
 
-    void Update()
+    void RappelingDown()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(keycodeForDown) && !characterController.isGrounded)
         {
-            isRappelling = !isRappelling; // Changing rappelling mode here 
+            isRappellingDown = !isRappellingDown; // Changing rappelling mode here 
+            isRappellingUp = false;
         }
 
-        if (isRappelling)
+        if (isRappellingDown)
         {
             Vector3 rappelMovement = Vector3.down * rappellingSpeed * Time.deltaTime;
             characterController.Move(rappelMovement);
@@ -28,9 +34,35 @@ public class RappellingRopeScript : MonoBehaviour
 
         if (characterController.isGrounded)
         {
-            isRappelling = false;
+            isRappellingDown = false;
             Debug.Log("Player reached the ground, stopping rappel.");
         }
+    }
 
+    void RappelingUp()
+    {
+        if (Input.GetKeyDown(keycodeForUp))
+        {
+            isRappellingUp = !isRappellingUp; // Changing rappelling mode here 
+            isRappellingDown = false;
+        }
+
+        if (isRappellingUp)
+        {
+            Vector3 rappelMovement = Vector3.up * rappellingSpeed * Time.deltaTime;
+            characterController.Move(rappelMovement);
+        }
+
+        if (transform.position.y >= initialPosition.y)
+        {
+            isRappellingUp = false;
+            Debug.Log("Player reached the top, stopping rappel.");
+        }
+    }
+
+    void Update()
+    {
+        RappelingDown();
+        RappelingUp();
     }
 }
